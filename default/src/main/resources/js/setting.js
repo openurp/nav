f1282a6819ac88ae7dfd190c50b7ae47751990ed
段体华 <duantihua@gmail.com>
2018-07-27 17:56:14 +0800

    fix #1 适配AdminLTE的导航风格

---
+++ default/src/main/resources/js/setting.js
@@ -0,0 +1,235 @@
+/**
+ * AdminLTE Demo Menu
+ */
+$(function () {
+    'use strict'
+
+    $('[data-toggle="control-sidebar"]').controlSidebar()
+    $('[data-toggle="push-menu"]').pushMenu()
+    var $pushMenu = $('[data-toggle="push-menu"]').data('lte.pushmenu')
+    var $controlSidebar = $('[data-toggle="control-sidebar"]').data('lte.controlsidebar')
+    var $layout = $('body').data('lte.layout')
+    $(window).on('load', function() {
+        // Reinitialize variables on load
+        $pushMenu = $('[data-toggle="push-menu"]').data('lte.pushmenu')
+        $controlSidebar = $('[data-toggle="control-sidebar"]').data('lte.controlsidebar')
+        $layout = $('body').data('lte.layout')
+    })
+
+    var mySkins = [
+        'skin-blue',
+        'skin-black',
+        'skin-red',
+        'skin-yellow',
+        'skin-purple',
+        'skin-green',
+        'skin-blue-light',
+        'skin-black-light',
+        'skin-red-light',
+        'skin-yellow-light',
+        'skin-purple-light',
+        'skin-green-light'
+    ]
+
+    function get(name) {
+        if (typeof (Storage) !== 'undefined') {
+            return localStorage.getItem(name)
+        } else {
+            window.alert('Please use a modern browser to properly view this template!')
+        }
+    }
+
+    function store(name, val) {
+        if (typeof (Storage) !== 'undefined') {
+            localStorage.setItem(name, val)
+        } else {
+            window.alert('Please use a modern browser to properly view this template!')
+        }
+    }
+
+    function changeLayout(cls) {
+        $('body').toggleClass(cls)
+        $layout.fixSidebar()
+        if ($('body').hasClass('fixed') && cls == 'fixed') {
+            $pushMenu.expandOnHover()
+            $layout.activate()
+        }
+        $controlSidebar.fix()
+    }
+
+    function changeSkin(cls) {
+        $.each(mySkins, function (i) {
+            $('body').removeClass(mySkins[i])
+        })
+
+        $('body').addClass(cls)
+        store('skin', cls)
+        return false
+    }
+    function setup() {
+        var tmp = get('skin')
+        if (tmp && $.inArray(tmp, mySkins))
+            changeSkin(tmp)
+
+        // Add the change skin listener
+        $('[data-skin]').on('click', function (e) {
+            if ($(this).hasClass('knob'))
+                return
+            e.preventDefault()
+            changeSkin($(this).data('skin'))
+        })
+
+        // Add the layout manager
+        $('[data-layout]').on('click', function () {
+            changeLayout($(this).data('layout'))
+        })
+
+        $('[data-controlsidebar]').on('click', function () {
+            changeLayout($(this).data('controlsidebar'))
+            var slide = !$controlSidebar.options.slide
+
+            $controlSidebar.options.slide = slide
+            if (!slide)
+                $('.control-sidebar').removeClass('control-sidebar-open')
+        })
+
+        //  Reset options
+        if ($('body').hasClass('fixed')) {
+            $('[data-layout="fixed"]').attr('checked', 'checked')
+        }
+    }
+
+    // Create the new tab
+    var $tabPane = $('<div />', {
+        'id': 'control-sidebar-theme-options-tab',
+        'class': 'tab-pane active'
+    })
+
+    // Create the tab button
+    var $tabButton = $('<li />', {'class': 'active'})
+        .html('<a href=\'#control-sidebar-theme-options-tab\' data-toggle=\'tab\'>'
+            + '<i class="fa fa-wrench"></i>'
+            + '</a>')
+
+    // Add the tab button to the right sidebar tabs
+    $('[href="#control-sidebar-home-tab"]')
+        .parent()
+        .before($tabButton)
+
+    // Create the menu
+    var $demoSettings = $('<div />')
+
+    // Layout options
+    $demoSettings.append(
+        '<h4 class="control-sidebar-heading">'
+        + '布局选项'
+        + '</h4>'
+        // Fixed layout
+        + '<div class="form-group">'
+        + '<label class="control-sidebar-subheading">'
+        + '<input type="checkbox"data-layout="fixed" class="pull-right"/> '
+        + '固定头部导航'
+        + '</label>'
+        + '</div>'
+    )
+    var $skinsList = $('<ul />', {'class': 'list-unstyled clearfix'})
+
+    // Dark sidebar skins
+    var $skinBlue =
+        $('<li />', {style: 'float:left; width: 33.33333%; padding: 5px;'})
+            .append('<a href="javascript:void(0)" data-skin="skin-blue" style="display: block; box-shadow: 0 0 3px rgba(0,0,0,0.4)" class="clearfix full-opacity-hover">'
+                + '<div><span style="display:block; width: 20%; float: left; height: 7px; background: #367fa9"></span><span class="bg-light-blue" style="display:block; width: 80%; float: left; height: 7px;"></span></div>'
+                + '<div><span style="display:block; width: 20%; float: left; height: 20px; background: #222d32"></span><span style="display:block; width: 80%; float: left; height: 20px; background: #f4f5f7"></span></div>'
+                + '</a>')
+    $skinsList.append($skinBlue)
+    var $skinBlack =
+        $('<li />', {style: 'float:left; width: 33.33333%; padding: 5px;'})
+            .append('<a href="javascript:void(0)" data-skin="skin-black" style="display: block; box-shadow: 0 0 3px rgba(0,0,0,0.4)" class="clearfix full-opacity-hover">'
+                + '<div style="box-shadow: 0 0 2px rgba(0,0,0,0.1)" class="clearfix"><span style="display:block; width: 20%; float: left; height: 7px; background: #fefefe"></span><span style="display:block; width: 80%; float: left; height: 7px; background: #fefefe"></span></div>'
+                + '<div><span style="display:block; width: 20%; float: left; height: 20px; background: #222"></span><span style="display:block; width: 80%; float: left; height: 20px; background: #f4f5f7"></span></div>'
+                + '</a>')
+    $skinsList.append($skinBlack)
+    var $skinPurple =
+        $('<li />', {style: 'float:left; width: 33.33333%; padding: 5px;'})
+            .append('<a href="javascript:void(0)" data-skin="skin-purple" style="display: block; box-shadow: 0 0 3px rgba(0,0,0,0.4)" class="clearfix full-opacity-hover">'
+                + '<div><span style="display:block; width: 20%; float: left; height: 7px;" class="bg-purple-active"></span><span class="bg-purple" style="display:block; width: 80%; float: left; height: 7px;"></span></div>'
+                + '<div><span style="display:block; width: 20%; float: left; height: 20px; background: #222d32"></span><span style="display:block; width: 80%; float: left; height: 20px; background: #f4f5f7"></span></div>'
+                + '</a>')
+    $skinsList.append($skinPurple)
+    var $skinGreen =
+        $('<li />', {style: 'float:left; width: 33.33333%; padding: 5px;'})
+            .append('<a href="javascript:void(0)" data-skin="skin-green" style="display: block; box-shadow: 0 0 3px rgba(0,0,0,0.4)" class="clearfix full-opacity-hover">'
+                + '<div><span style="display:block; width: 20%; float: left; height: 7px;" class="bg-green-active"></span><span class="bg-green" style="display:block; width: 80%; float: left; height: 7px;"></span></div>'
+                + '<div><span style="display:block; width: 20%; float: left; height: 20px; background: #222d32"></span><span style="display:block; width: 80%; float: left; height: 20px; background: #f4f5f7"></span></div>'
+                + '</a>')
+    $skinsList.append($skinGreen)
+    var $skinRed =
+        $('<li />', {style: 'float:left; width: 33.33333%; padding: 5px;'})
+            .append('<a href="javascript:void(0)" data-skin="skin-red" style="display: block; box-shadow: 0 0 3px rgba(0,0,0,0.4)" class="clearfix full-opacity-hover">'
+                + '<div><span style="display:block; width: 20%; float: left; height: 7px;" class="bg-red-active"></span><span class="bg-red" style="display:block; width: 80%; float: left; height: 7px;"></span></div>'
+                + '<div><span style="display:block; width: 20%; float: left; height: 20px; background: #222d32"></span><span style="display:block; width: 80%; float: left; height: 20px; background: #f4f5f7"></span></div>'
+                + '</a>')
+    $skinsList.append($skinRed)
+    var $skinYellow =
+        $('<li />', {style: 'float:left; width: 33.33333%; padding: 5px;'})
+            .append('<a href="javascript:void(0)" data-skin="skin-yellow" style="display: block; box-shadow: 0 0 3px rgba(0,0,0,0.4)" class="clearfix full-opacity-hover">'
+                + '<div><span style="display:block; width: 20%; float: left; height: 7px;" class="bg-yellow-active"></span><span class="bg-yellow" style="display:block; width: 80%; float: left; height: 7px;"></span></div>'
+                + '<div><span style="display:block; width: 20%; float: left; height: 20px; background: #222d32"></span><span style="display:block; width: 80%; float: left; height: 20px; background: #f4f5f7"></span></div>'
+                + '</a>')
+    $skinsList.append($skinYellow)
+
+    
+    // Light sidebar skins
+    var $skinBlueLight =
+        $('<li />', {style: 'float:left; width: 33.33333%; padding: 5px;'})
+            .append('<a href="javascript:void(0)" data-skin="skin-blue-light" style="display: block; box-shadow: 0 0 3px rgba(0,0,0,0.4)" class="clearfix full-opacity-hover">'
+                + '<div><span style="display:block; width: 20%; float: left; height: 7px; background: #367fa9"></span><span class="bg-light-blue" style="display:block; width: 80%; float: left; height: 7px;"></span></div>'
+                + '<div><span style="display:block; width: 20%; float: left; height: 20px; background: #f9fafc"></span><span style="display:block; width: 80%; float: left; height: 20px; background: #f4f5f7"></span></div>'
+                + '</a>')
+    $skinsList.append($skinBlueLight)
+    var $skinBlackLight =
+        $('<li />', {style: 'float:left; width: 33.33333%; padding: 5px;'})
+            .append('<a href="javascript:void(0)" data-skin="skin-black-light" style="display: block; box-shadow: 0 0 3px rgba(0,0,0,0.4)" class="clearfix full-opacity-hover">'
+                + '<div style="box-shadow: 0 0 2px rgba(0,0,0,0.1)" class="clearfix"><span style="display:block; width: 20%; float: left; height: 7px; background: #fefefe"></span><span style="display:block; width: 80%; float: left; height: 7px; background: #fefefe"></span></div>'
+                + '<div><span style="display:block; width: 20%; float: left; height: 20px; background: #f9fafc"></span><span style="display:block; width: 80%; float: left; height: 20px; background: #f4f5f7"></span></div>'
+                + '</a>')
+    $skinsList.append($skinBlackLight)
+    var $skinPurpleLight =
+        $('<li />', {style: 'float:left; width: 33.33333%; padding: 5px;'})
+            .append('<a href="javascript:void(0)" data-skin="skin-purple-light" style="display: block; box-shadow: 0 0 3px rgba(0,0,0,0.4)" class="clearfix full-opacity-hover">'
+                + '<div><span style="display:block; width: 20%; float: left; height: 7px;" class="bg-purple-active"></span><span class="bg-purple" style="display:block; width: 80%; float: left; height: 7px;"></span></div>'
+                + '<div><span style="display:block; width: 20%; float: left; height: 20px; background: #f9fafc"></span><span style="display:block; width: 80%; float: left; height: 20px; background: #f4f5f7"></span></div>'
+                + '</a>')
+    $skinsList.append($skinPurpleLight)
+    var $skinGreenLight =
+        $('<li />', {style: 'float:left; width: 33.33333%; padding: 5px;'})
+            .append('<a href="javascript:void(0)" data-skin="skin-green-light" style="display: block; box-shadow: 0 0 3px rgba(0,0,0,0.4)" class="clearfix full-opacity-hover">'
+                + '<div><span style="display:block; width: 20%; float: left; height: 7px;" class="bg-green-active"></span><span class="bg-green" style="display:block; width: 80%; float: left; height: 7px;"></span></div>'
+                + '<div><span style="display:block; width: 20%; float: left; height: 20px; background: #f9fafc"></span><span style="display:block; width: 80%; float: left; height: 20px; background: #f4f5f7"></span></div>'
+                + '</a>')
+    $skinsList.append($skinGreenLight)
+    var $skinRedLight =
+        $('<li />', {style: 'float:left; width: 33.33333%; padding: 5px;'})
+            .append('<a href="javascript:void(0)" data-skin="skin-red-light" style="display: block; box-shadow: 0 0 3px rgba(0,0,0,0.4)" class="clearfix full-opacity-hover">'
+                + '<div><span style="display:block; width: 20%; float: left; height: 7px;" class="bg-red-active"></span><span class="bg-red" style="display:block; width: 80%; float: left; height: 7px;"></span></div>'
+                + '<div><span style="display:block; width: 20%; float: left; height: 20px; background: #f9fafc"></span><span style="display:block; width: 80%; float: left; height: 20px; background: #f4f5f7"></span></div>'
+                + '</a>')
+    $skinsList.append($skinRedLight)
+    var $skinYellowLight =
+        $('<li />', {style: 'float:left; width: 33.33333%; padding: 5px;'})
+            .append('<a href="javascript:void(0)" data-skin="skin-yellow-light" style="display: block; box-shadow: 0 0 3px rgba(0,0,0,0.4)" class="clearfix full-opacity-hover">'
+                + '<div><span style="display:block; width: 20%; float: left; height: 7px;" class="bg-yellow-active"></span><span class="bg-yellow" style="display:block; width: 80%; float: left; height: 7px;"></span></div>'
+                + '<div><span style="display:block; width: 20%; float: left; height: 20px; background: #f9fafc"></span><span style="display:block; width: 80%; float: left; height: 20px; background: #f4f5f7"></span></div>'
+                + '</a>')
+    $skinsList.append($skinYellowLight)
+
+    $demoSettings.append('<h4 class="control-sidebar-heading">皮肤</h4>')
+    $demoSettings.append($skinsList)
+
+    $tabPane.append($demoSettings)
+    $('#control-sidebar-home-tab').after($tabPane)
+
+    setup()
+
+    $('[data-toggle="tooltip"]').tooltip()
+})
